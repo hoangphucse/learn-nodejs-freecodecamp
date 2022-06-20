7c748682fa0ff9f544950236b46e2aed356198e3
hoangphuc <phucherose@gmail.com>
2022-06-20 22:27:12 +0700

    first commit

---
+++ 03-task-manager/starter/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+// Middleware
+
+app.use(express.json());
+
+// Routes
+app.get('/hello', (req, res) => {
+  res.send('Task manager app');
+});
+
+app.use('/api/v1/tasks', tasks);
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening on port ${port}...`));
