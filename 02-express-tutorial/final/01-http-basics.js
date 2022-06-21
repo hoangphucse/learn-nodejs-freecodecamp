ddcd71db3bd97b58c30e97301b63a7dc262cc149
Phuc Tran <thphuc2@tma.com.vn>
2022-06-21 14:03:57 +0700

    test

--- 02-express-tutorial/final/01-http-basics.js
+++ 02-express-tutorial/final/01-http-basics.js
@@ -9,10 +9,10 @@ const server = http.createServer((req, res) => {
     res.write('<h1>home page</h1>')
     res.end()
   }
-  // about page
+        // about page
   else if (url === '/about') {
     res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
+    res.write('<h1>about page 213123</h1>')
     res.end()
   }
   // 404
