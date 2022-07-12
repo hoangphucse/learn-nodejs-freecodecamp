bb8249656968392cc410df4125f6c0a406b1dcfd
Phuc Tran <thphuc2@tma.com.vn>
2022-07-12 17:21:20 +0700

    basic upload file

---
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -0,0 +1,39 @@
+const Product = require('../models/Product');
+const path = require('path');
+const { StatusCodes } = require('http-status-codes');
+const CustomAPIError = require('../errors');
+
+const uploadProductImage = async (req, res) => {
+  // check if file exists
+  if (!req.files) {
+    throw new CustomAPIError.BadRequestError('No file Uploaded');
+  }
+
+  const productImage = req.files.image;
+  // check format
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomAPIError.BadRequestError('Please Upload Image');
+  }
+
+  // check size
+  const maxSize = 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new CustomAPIError.BadRequestError(
+      'Please Upload Image Smaller than 1MB'
+    );
+  }
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  );
+  await productImage.mv(imagePath);
+
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+module.exports = {
+  uploadProductImage,
+};
