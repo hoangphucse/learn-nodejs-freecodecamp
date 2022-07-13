a3475228b360ec31dd1f226010c0f49475bcb4e9
Phuc Tran <thphuc2@tma.com.vn>
2022-07-13 09:34:16 +0700

    completed project

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { StatusCodes } = require('http-status-codes');
 const CustomAPIError = require('../errors');
 const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
 const uploadProductImageLocal = async (req, res) => {
   // check if file exists
@@ -35,9 +36,20 @@ const uploadProductImageLocal = async (req, res) => {
     .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
-const uploadProductImage = async () => {
-  
-  const result = await cloudinary.uploader.upload()
+const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: '04-file-upload',
+    }
+  );
+  fs.unlinkSync(req.files.image.tempFilePath);
+  return res.status(StatusCodes.OK).json({
+    image: {
+      src: result.secure_url,
+    },
+  });
 };
 
 module.exports = {
